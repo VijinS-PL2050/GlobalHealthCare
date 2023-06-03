bbbf056c63d244abfc2e389c1c9e1b8bbdb534a1
shalini <baijushalini135@gmail.com>
2023-06-03 22:49:09 +0530

    13th commit by shalini...created medicine stock table

--- src/main/java/com/global/entity/MedicinesStock.java
+++ src/main/java/com/global/entity/MedicinesStock.java
@@ -5,7 +5,15 @@ import javax.persistence.Entity;
 @Entity
 public class MedicinesStock {
 	
+
 	private int medicineId;
+	private String medicineName;
+	private String companyName;
+	private  String genericName;
+	private int quantity;
+	private double price;
+	private String isActive;
+	
 	
 
 }
