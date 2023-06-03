d188da467b29ec8b4efcc054262ee559a3fffebe
mebeena <mebeena26@gmail.com>
2023-06-03 22:01:10 +0530

    10th commit staffTable &DoctorDetailstable in entity

---
+++ src/main/java/com/global/entity/DoctorDetails.java
@@ -0,0 +1,150 @@
+package com.global.entity;
+
+import java.time.LocalDate;
+
+public class DoctorDetails {
+
+	private int id;
+	private String doctorid;
+	private String doctorname;
+	private LocalDate dateofbirth;
+	private String gender;
+	private String bloodgroup;
+	private String phoneno;
+	private String address;
+	private String adharNo;
+	private String qualification;
+	private String email;
+	private String specialization;
+	private double consultancyfee;
+	private String IsActive;
+	
+	public DoctorDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDoctorid() {
+		return doctorid;
+	}
+
+	public void setDoctorid(String doctorid) {
+		this.doctorid = doctorid;
+	}
+
+	public String getDoctorname() {
+		return doctorname;
+	}
+
+	public void setDoctorname(String doctorname) {
+		this.doctorname = doctorname;
+	}
+
+	public LocalDate getDateofbirth() {
+		return dateofbirth;
+	}
+
+	public void setDateofbirth(LocalDate dateofbirth) {
+		this.dateofbirth = dateofbirth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getBloodgroup() {
+		return bloodgroup;
+	}
+
+	public void setBloodgroup(String bloodgroup) {
+		this.bloodgroup = bloodgroup;
+	}
+
+	public String getPhoneno() {
+		return phoneno;
+	}
+
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAdharNo() {
+		return adharNo;
+	}
+
+	public void setAdharNo(String adharNo) {
+		this.adharNo = adharNo;
+	}
+
+	public String getQualification() {
+		return qualification;
+	}
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+
+	public double getConsultancyfee() {
+		return consultancyfee;
+	}
+
+	public void setConsultancyfee(double consultancyfee) {
+		this.consultancyfee = consultancyfee;
+	}
+
+	public String getIsActive() {
+		return IsActive;
+	}
+
+	public void setIsActive(String isActive) {
+		IsActive = isActive;
+	}
+
+	@Override
+	public String toString() {
+		return "DoctorDetails [id=" + id + ", doctorid=" + doctorid + ", doctorname=" + doctorname + ", dateofbirth="
+				+ dateofbirth + ", gender=" + gender + ", bloodgroup=" + bloodgroup + ", phoneno=" + phoneno
+				+ ", address=" + address + ", adharNo=" + adharNo + ", qualification=" + qualification + ", email="
+				+ email + ", specialization=" + specialization + ", consultancyfee=" + consultancyfee + ", IsActive="
+				+ IsActive + "]";
+	}
+
+	
+	
+}
---
+++ src/main/java/com/global/entity/Staff.java
@@ -0,0 +1,160 @@
+package com.global.entity;
+
+import java.time.LocalDate;
+
+public class Staff {
+	
+	private int id;
+	private String staffid;
+	private String staffname;
+	//relation to usertable(userid foreign key)
+	private String userId;
+	private LocalDate dateofbirth;
+	private String gender;
+	private String bloodgroup;
+	private String phoneno;
+	private String address;
+	private String adharNo;
+	private String qualification;
+	private String email;
+	private String IsActive;
+	
+	
+	public Staff() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getStaffid() {
+		return staffid;
+	}
+
+
+	public void setStaffid(String staffid) {
+		this.staffid = staffid;
+	}
+
+
+	public String getStaffname() {
+		return staffname;
+	}
+
+
+	public void setStaffname(String staffname) {
+		this.staffname = staffname;
+	}
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+	public LocalDate getDateofbirth() {
+		return dateofbirth;
+	}
+
+
+	public void setDateofbirth(LocalDate dateofbirth) {
+		this.dateofbirth = dateofbirth;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+	public String getBloodgroup() {
+		return bloodgroup;
+	}
+
+
+	public void setBloodgroup(String bloodgroup) {
+		this.bloodgroup = bloodgroup;
+	}
+
+
+	public String getPhoneno() {
+		return phoneno;
+	}
+
+
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getAdharNo() {
+		return adharNo;
+	}
+
+
+	public void setAdharNo(String adharNo) {
+		this.adharNo = adharNo;
+	}
+
+
+	public String getQualification() {
+		return qualification;
+	}
+
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getIsActive() {
+		return IsActive;
+	}
+
+
+	public void setIsActive(String isActive) {
+		IsActive = isActive;
+	}
+	
+	
+
+}
